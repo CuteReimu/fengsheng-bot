dbe90f780abd7f030085344e2e3f0c21641f787d
CuteReimu <415551921@qq.com>
2023-08-24 15:16:55 +0800

    优化代码

--- src/main/kotlin/utils/HttpUtil.kt
+++ src/main/kotlin/utils/HttpUtil.kt
@@ -78,7 +78,8 @@ object HttpUtil {
     private const val ua =
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.69"
     private val client =
-        OkHttpClient().newBuilder().followRedirects(false).connectTimeout(Duration.ofMillis(20000)).build()
+        OkHttpClient().newBuilder().followRedirects(false).connectTimeout(Duration.ofMillis(20000))
+            .callTimeout(Duration.ofMillis(20000)).build()
     private val json = Json {
         prettyPrint = true
         ignoreUnknownKeys = true
