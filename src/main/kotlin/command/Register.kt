9f9b9cdceb7986bfdd4a11ea466924bfc3af81b5
CuteReimu <415551921@qq.com>
2023-09-05 11:02:11 +0800

    注册功能拦截非法字符

--- src/main/kotlin/command/Register.kt
+++ src/main/kotlin/command/Register.kt
@@ -6,6 +6,7 @@ import com.fengsheng.bot.utils.HttpUtil
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.message.data.Message
 import net.mamoe.mirai.message.data.PlainText
+import net.mamoe.mirai.message.data.toPlainText
 
 object Register : CommandHandler {
     override val name = "注册"
@@ -16,12 +17,16 @@ object Register : CommandHandler {
 
     override fun checkAuth(groupCode: Long, senderId: Long) = true
 
-    override suspend fun execute(msg: GroupMessageEvent, content: String): Message {
+    override suspend fun execute(msg: GroupMessageEvent, content: String): Message? {
         val name = content.trim()
         if (name.isEmpty()) return PlainText("命令格式：\n注册 名字")
         val oldName = PermData.playerMap[msg.sender.id]
         if (oldName != null) return PlainText("你已经注册过：$oldName")
-        if (!HttpUtil.register(name)) return PlainText("用户名重复")
+        try {
+            if (!HttpUtil.register(name)) return PlainText("用户名重复")
+        } catch (e: Exception) {
+            return e.message?.toPlainText()
+        }
         synchronized(PermData) {
             PermData.playerMap += msg.sender.id to name
             Bind.reversePlayerMap += name to msg.sender.id
