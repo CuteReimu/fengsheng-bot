980a1e2c0ab00e3c46751b1c376da88e82ab0c00
CuteReimu <415551921@qq.com>
2024-05-08 18:24:08 +0800

    解绑所有0分玩家功能优化

--- src/main/kotlin/command/UnbindExpired.kt
+++ src/main/kotlin/command/UnbindExpired.kt
@@ -18,8 +18,7 @@ object UnbindExpired : CommandHandler {
     override suspend fun execute(msg: GroupMessageEvent, content: String): Message? {
         content.isBlank() || return null
         for (name in PermData.playerMap.values.toList()) {
-            val s = HttpUtil.getScore(name)
-            if ("·0，总场次：0，" in s || s.endsWith("已身死道消")) {
+            if (HttpUtil.getScore(name).endsWith("已身死道消")) {
                 val id = Bind.reversePlayerMap[name] ?: continue
                 synchronized(PermData) {
                     PermData.playerMap -= id
